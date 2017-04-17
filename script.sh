d2044ebb6f83f61c0564c56fee79deaf003cf2c0
Diego Luiz <diegorcl@gmail.com>
2017-04-17 15:30:55 +0100

    Added dict devhelp

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade 
-sudo apt install -y guake synapse zsh git vim virtualbox python-pip vagrant docker.io golang nodejs r-base libjpeg62 redis-tools mpg123 nmap libxml2-utils ruby ruby-dev make gcc
+sudo apt install -y guake synapse zsh git vim virtualbox python-pip vagrant docker.io golang nodejs r-base libjpeg62 redis-tools mpg123 nmap libxml2-utils ruby ruby-dev make gcc dict devhelp
 
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 

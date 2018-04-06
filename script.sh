a7aae15f93fed7dde88dbd9746d2a66f2b79de88
Diego Luiz <@email>
2018-04-06 23:32:04 +0100

    upgraded the script

--- script.sh
+++ script.sh
@@ -1,53 +1,55 @@
 #!/bin/bash
 
+echo generating new ssh keys
+ssh-keygen 
+
+echo apt update and upgrade
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y guake synapse zsh git vim virtualbox python-pip vagrant docker.io golang nodejs r-base libjpeg62 redis-tools mpg123 nmap libxml2-utils ruby ruby-dev make gcc dict devhelp htop
 
+echo install some packages
+sudo apt install -y synapse zsh git vim python-pip python3-pip vagrant docker.io golang r-base libjpeg62 redis-tools mpg123 nmap libxml2-utils ruby ruby-dev make gcc dict devhelp htop tor
+
+echo install oh my zsh
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-cd ~
-mkdir tmp-script
-cd tmp-script
-rm *
+echo install terraform
+wget https://releases.hashicorp.com/terraform/0.11.6/terraform_0.11.6_linux_amd64.zip?_ga=2.8038967.406755400.1523051101-1822120829.1523051101
+unzip terraform_0.11.6_linux_amd64.zip?_ga=2.8038967.406755400.1523051101-1822120829.1523051101
+sudo mv terraform /usr/local/bin/
 
-# Downloads and installs Visual Studio Code
-wget https://raw.githubusercontent.com/diegoluiz/mint-setup/master/code_1.11.2-1492070517_amd64.deb 
-sudo dpkg -i code_1.11.2-1492070517_amd64.deb 
-rm *
-
-# Downloads and installs Chrome 
-wget https://raw.githubusercontent.com/diegoluiz/mint-setup/master/google-chrome-stable_current_amd64.deb 
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm *
-
-# Downloads and installs RStudio
-wget https://raw.githubusercontent.com/diegoluiz/mint-setup/master/rstudio-1.0.136-amd64.deb 
-sudo dpkg -i rstudio-1.0.136-amd64.deb 
-rm *
-
-# Install Franz
-wget https://raw.githubusercontent.com/diegoluiz/mint-setup/master/Franz-linux-x64-4.0.4.tgz
-mkdir franz 
-cd franz
-tar -xvzf ../Franz-linux-x64-4.0.4.tgz .
-cd ..
-sudo mv franz /opt/
-sudo chown root.root -R /opt/franz
-sudo ln -fs /opt/franz/Franz /usr/bin/franz
-rmdir franz
-rm *
-
-# .gitconfig
-wget https://raw.githubusercontent.com/diegoluiz/mint-setup/master/.gitconfig 
-cp -f .gitconfig ~
-rm *
-
-# sounds aliases
-wget https://raw.githubusercontent.com/diegoluiz/emotion_sounds/master/sounds.sh
-cat sounds.sh >> ~/.zshrc
+echo install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 source ~/.zshrc
-rm *
+nvm install 8.10.0
+npm install -g --upgrade npm
 
-cd ..
-rmdir tmp-script
+echo installs Visual Studio Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install -y code
+
+echo install cloud clis
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y google-cloud-sdk google-cloud-sdk-app-engine-python google-cloud-sdk-app-engine-python-extras google-cloud-sdk-app-engine-java google-cloud-sdk-app-engine-go google-cloud-sdk-datalab google-cloud-sdk-datastore-emulator google-cloud-sdk-pubsub-emulator google-cloud-sdk-cbt google-cloud-sdk-bigtable-emulator kubectl
+pip install awscli --upgrade --user
+echo 'export PATH=~/.local/bin:$PATH' >> ~/.zshrc
+
+echo installs RStudio
+wget https://download1.rstudio.org/rstudio-xenial-1.1.442-amd64.deb
+sudo dpkg -i rstudio-xenial-1.1.442-amd64.deb 
+sudo rm rstudio-xenial-1.1.442-amd64.deb 
+
+echo install Franz
+wget https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.17/franz_5.0.0-beta.17_amd64.deb
+sudo dkpg -i franz_5.0.0-beta.17_amd64.deb
+sudo rm franz_5.0.0-beta.17_amd64.deb
+
+echo copy .gitconfig
+wget https://raw.githubusercontent.com/diegoluiz/mint-setup/master/.gitconfig 
+mv .gitconfig ~
